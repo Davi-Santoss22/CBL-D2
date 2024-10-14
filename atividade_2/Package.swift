67625c1678a45b2c879fc19cd64776052d5a5577
Davi Santos <davidosantosqueiroz@outlook.com>
2024-10-14 13:06:57 -0300

    Update Package.swift
    Alterando os valores das variáveis que estavam recebendo "atividade_3" por "atividade_2".

--- atividade_2/Package.swift
+++ atividade_2/Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "atividade_3",
+    name: "atividade_2",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "atividade_3"),
+            name: "atividade_2"),
     ]
 )
